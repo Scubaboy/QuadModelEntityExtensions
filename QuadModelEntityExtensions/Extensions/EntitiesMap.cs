80cd3bfffd634cad479c7ee4fdeb8862f54f22af
Scubaboy <robin@robincawsey.plus.com>
2015-06-02 12:28:49 +0100

    extension methods complete

---
+++ QuadModelEntityExtensions/Extensions/EntitiesMap.cs
@@ -0,0 +1,24 @@
+﻿using AutoMapper;
+using QuadEFModels.Entities;
+using QuadModels;
+
+namespace QuadModelEntityExtensions.Extensions
+{
+    public static class EntitiesMap
+    {
+        static EntitiesMap()
+        {
+            Mapper.CreateMap<ActiveQuadEntity, ActiveQuad>()
+                .ConstructUsing(x => new ActiveQuad(x.ActiveQuadEntityId));
+
+            Mapper.AssertConfigurationIsValid();
+        }
+
+        public static ActiveQuad ToModel(this ActiveQuadEntity activeQuad)
+        {
+            return activeQuad == null
+                ? null
+                : Mapper.Map<ActiveQuadEntity, ActiveQuad>(activeQuad);
+        }
+    }
+}
---
+++ QuadModelEntityExtensions/Extensions/ModelMaps.cs
@@ -0,0 +1,24 @@
+﻿using AutoMapper;
+using QuadEFModels.Entities;
+using QuadModels;
+
+namespace QuadModelEntityExtensions.Extensions
+{
+    public static class ModelsMap
+    {
+        static ModelsMap()
+        {
+            Mapper.CreateMap<ActiveQuad, ActiveQuadEntity>()
+                .ForMember(x => x.ActiveQuadEntityId, opts => opts.MapFrom(src => src.Id));
+
+            Mapper.AssertConfigurationIsValid();
+        }
+
+        public static ActiveQuadEntity ToEntity(this ActiveQuad activeQuad)
+        {
+            return activeQuad == null
+                ? null
+                : Mapper.Map<ActiveQuad, ActiveQuadEntity>(activeQuad);
+        }
+    }
+}
